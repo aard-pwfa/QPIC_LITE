9cc7c0f11923c4b2594cbc0130a53e0ccf4d2a64
sgess <spencergessner@gmail.com>
2012-09-07 16:12:43 -0700

    rp maker up to date

--- CALC_RP.m
+++ CALC_RP.m
@@ -1,179 +1,215 @@
-function [output_struct] = CALC_RP(input_struct)
+function param_struct = CALC_RP(input_struct)
 
 % import standard SI constants
 eval(['run ' pwd '/SI_consts.m']);
 
-% Calc plasma parameters
-output_struct.omega_p  = sqrt(input_struct.plasma_density*1e6*SI_e^2/(SI_em*SI_eps0));  % plasma frequency  [rad/s]
-output_struct.lambda_p = 2*pi*SI_c*1e6/output_struct.omega_p;                           % plasma wavelength [um]
-output_struct.k_p      = output_struct.omega_p/SI_c;                                    % plasma wavenumber [1/m]
-output_struct.SD       = 1e6/output_struct.k_p;                                         % plasma skin depth [um]
 
-% Transfer used parameters
-output_struct.plasma_density = input_struct.plasma_density;
+
+%%%%%%%%%%%%%%%%%%%%%
+% PLASMA ATTRIBUTES %
+%%%%%%%%%%%%%%%%%%%%%
+
+param_struct.plasma.density  = input_struct.plasma.density; % plasma density [cm^-3]
+param_struct.plasma.charge   = input_struct.plasma.charge;  % plasma particle charge [e]
+param_struct.plasma.mass     = input_struct.plasma.mass;    % plasma particle mass [e_m]
+param_struct.plasma.PREION   = input_struct.plasma.PREION;  % 0 : non-ionized plasma 1: pre-ionized plasma
+param_struct.plasma.Z        = input_struct.plasma.Z;       % ion number
+
+% Calc plasma parameters
+param_struct.plasma.omega_p  = sqrt(param_struct.plasma.density*...
+    1e6*SI_e^2/(SI_em*SI_eps0));                                                    % plasma frequency  [rad/s]
+param_struct.plasma.lambda_p = 2*pi*SI_c*1e6/param_struct.plasma.omega_p;           % plasma wavelength [um]
+param_struct.plasma.k_p      = param_struct.plasma.omega_p/SI_c;                    % plasma wavenumber [1/m]
+param_struct.plasma.SD       = 1e6*SI_c/param_struct.plasma.omega_p;                % plasma skin depth [um]
+param_struct.plasma.time     = 1/param_struct.plasma.omega_p;                       % characteristic time scale [s]
+param_struct.plasma.rqm      = param_struct.plasma.mass/param_struct.plasma.charge; % mass to charge ratio
+
+
+
+%%%%%%%%%%%%%%%%%%%
+% BEAM ATTRIBUTES %
+%%%%%%%%%%%%%%%%%%%
+
+param_struct.beam.charge      = input_struct.beam.charge;                        % beam particle charge [e]
+param_struct.beam.mass        = input_struct.beam.mass;                          % beam particle mass [e_m]
+param_struct.beam.N_particles = input_struct.beam.N_particles;                   % number of beam particles
+param_struct.beam.rqm         = param_struct.beam.mass/param_struct.beam.charge; % mass to charge ratio
 
 % Calc gamma if not specified
-if input_struct.gamma == 0
-   if input_struct.energy == 0
+if input_struct.beam.gamma == 0
+   if input_struct.beam.energy == 0
       error('Must specify either gamma or energy');
    end
-   output_struct.gamma  = input_struct.energy/(input_struct.mass*SI_eM/1e3);
-   output_struct.energy = input_struct.energy;
+   param_struct.beam.gamma  = input_struct.beam.energy/...
+       (input_struct.beam.mass*SI_eM/1e3);                  % beam gamma
+   param_struct.beam.energy = input_struct.beam.energy;     % beam energy [GeV]
 end
 
 % Calc energy if not specified
-if input_struct.energy == 0
-   if input_struct.gamma == 0
+if input_struct.beam.energy == 0
+   if input_struct.beam.gamma == 0
       error('Must specify either gamma or energy');
    end
-   output_struct.energy = input_struct.gamma*input_struct.mass*SI_eM/1e3;
-   output_struct.gamma  = input_struct.gamma;
+   param_struct.beam.energy = input_struct.beam.gamma*...
+       input_struct.beam.mass*SI_eM/1e3;                    % beam energy [GeV]
+   param_struct.beam.gamma  = input_struct.beam.gamma;      % beam gamma
 end
 
 % Betatron frequecy, wavelength
-output_struct.omega_b = sqrt(output_struct.plasma_density*1e6*SI_e^2/...
-    (2*output_struct.gamma*input_struct.mass*SI_em*SI_eps0));
-output_struct.lambda_b = 2*pi*SI_c*1e6/output_struct.omega_b;
-
-% % Calc beam size if beam_match == 1
-% if( input_struct.beam_match )
-%   output_struct.sigma_x = 1e3*sqrt(input_struct.emit_x/output_struct.k_p*sqrt(2/output_struct.gamma)); % [um]
-%   output_struct.sigma_y = 1e3*sqrt(input_struct.emit_y/output_struct.k_p*sqrt(2/output_struct.gamma)); % [um]
-%   output_struct.emit_x  = input_struct.emit_x; % [mm*mrad]
-%   output_struct.emit_y  = input_struct.emit_x; % [mm*mrad]
-% end
-% 
-% % Calc emittance if emit_match == 1
-% if( input_struct.emit_match )
-%   output_struct.emit_x  = 1e-6*output_struct.k_p*(input_struct.sigma_x)^2*sqrt(output_struct.gamma/2); % [mm*mrad]
-%   output_struct.emit_y  = 1e-6*output_struct.k_p*(input_struct.sigma_y)^2*sqrt(output_struct.gamma/2); % [mm*mrad]
-%   output_struct.sigma_x = input_struct.sigma_x; % [um]
-%   output_struct.sigma_y = input_struct.sigma_y; % [um]
-% end
+param_struct.beam.omega_b  = sqrt(param_struct.plasma.density*1e6*SI_e^2/... 
+    (2*param_struct.beam.gamma*param_struct.beam.mass*SI_em*SI_eps0));     % betatron frequency [rad/s]
+param_struct.beam.lambda_b = 2*pi*SI_c*1e6/param_struct.beam.omega_b;        % betatron wavelength [um]
 
 % Calc beam size if beam_match == 1
-if( input_struct.beam_match )
-  output_struct.sigma_x = 1e3*sqrt(input_struct.emit_x/...
-      output_struct.k_p*sqrt(2/output_struct.gamma));      % beam size X [um]
+if( input_struct.beam.beam_match )
+  param_struct.beam.sigma_x = 1e3*sqrt(input_struct.beam.emit_x/...
+      param_struct.plamsa.k_p*sqrt(2/param_struct.beam.gamma));      % beam size X [um]
   
-  output_struct.sigma_y = 1e3*sqrt(input_struct.beam.emit_y/...
-      output_struct.k_p*sqrt(2/output_struct.gamma));      % beam size Y [um]
+  param_struct.beam.sigma_y = 1e3*sqrt(input_struct.beam.emit_y/...
+      param_struct.plasma.k_p*sqrt(2/param_struct.beam.gamma));      % beam size Y [um]
   
-  output_struct.emit_x  = input_struct.emit_x;              % beam norm X emmitance [mm*mrad]
-  output_struct.emit_y  = input_struct.emit_x;              % beam norm Y emmitance [mm*mrad]
+  param_struct.beam.emit_x  = input_struct.beam.emit_x;              % beam norm X emmitance [mm*mrad]
+  param_struct.beam.emit_y  = input_struct.beam.emit_x;              % beam norm Y emmitance [mm*mrad]
 
 % Calc emittance if emit_match == 1
-elseif( input_struct.emit_match )
-  output_struct.emit_x  = 1e-6*output_struct.k_p*...
-      (input_struct.sigma_x)^2*sqrt(output_struct.gamma/2); % beam norm X emmitance [mm*mrad]
-  output_struct.emit_y  = 1e-6*output_struct.k_p*...
-      (input_struct.sigma_y)^2*sqrt(output_struct.gamma/2); % beam norm Y emmitance [mm*mrad]
+elseif( input_struct.beam.emit_match )
+  param_struct.beam.emit_x  = 1e-6*param_struct.plasma.k_p*...
+      (input_struct.beam.sigma_x)^2*sqrt(param_struct.beam.gamma/2); % beam norm X emmitance [mm*mrad]
+  param_struct.beam.emit_y  = 1e-6*param_struct.plasma.k_p*...
+      (input_struct.beam.sigma_y)^2*sqrt(param_struct.beam.gamma/2); % beam norm Y emmitance [mm*mrad]
   
-  output_struct.sigma_x = input_struct.sigma_x;             % beam size X [um]
-  output_struct.sigma_y = input_struct.sigma_y;             % beam size Y [um]
+  param_struct.beam.sigma_x = input_struct.beam.sigma_x;             % beam size X [um]
+  param_struct.beam.sigma_y = input_struct.beam.sigma_y;             % beam size Y [um]
 
 else
-  output_struct.sigma_x = input_struct.sigma_x;             % beam size X [um]
-  output_struct.sigma_y = input_struct.sigma_y;             % beam size Y [um]
+  param_struct.beam.sigma_x = input_struct.beam.sigma_x;             % beam size X [um]
+  param_struct.beam.sigma_y = input_struct.beam.sigma_y;             % beam size Y [um]
   
-  output_struct.emit_x  = input_struct.emit_x;              % beam norm X emmitance [mm*mrad]
-  output_struct.emit_y  = input_struct.emit_x;              % beam norm Y emmitance [mm*mrad]
+  param_struct.beam.emit_x  = input_struct.beam.emit_x;              % beam norm X emmitance [mm*mrad]
+  param_struct.beam.emit_y  = input_struct.beam.emit_x;              % beam norm Y emmitance [mm*mrad]
 end
 
+% Normalized beam divergence
+param_struct.beam.angle_x = param_struct.beam.emit_x/param_struct.beam.sigma_x; % emit_x/sigma_x
+param_struct.beam.angle_y = param_struct.beam.emit_y/param_struct.beam.sigma_y; % emit_x/sigma_x
+
 % Calc sigma_z if z_match == 1
-if( input_struct.z_match )
-   output_struct.sigma_z = 1e6*sqrt(2)/output_struct.k_p; % [um]
+if( input_struct.beam.z_match )
+   param_struct.beam.sigma_z = 1e6*sqrt(2)*param_struct.plasma.SD;   % beam size Z [um]
 else
-   output_struct.sigma_z = input_struct.sigma_z;
+   param_struct.beam.sigma_z = input_struct.beam.sigma_z;            % beam size Z [um]
 end
 
 % Calc beam density
-output_struct.beam_density = (10000^3)*input_struct.N_particles/...
-    ((2*pi)^(3/2)*output_struct.sigma_x*output_struct.sigma_y*output_struct.sigma_z);
+param_struct.beam.density = (10000^3)*param_struct.beam.N_particles/...
+    ((2*pi)^(3/2)*param_struct.beam.sigma_x*param_struct.beam.sigma_y*...
+    param_struct.beam.sigma_z);                                           % beam density [cm^-3]
 
 % Calc N_b/N_p
-output_struct.ratio = output_struct.beam_density/output_struct.plasma_density;
+param_struct.beam.ratio = param_struct.beam.density/param_struct.plasma.density; % beam density [n0]
 
 % Calc k_p*sigma_x,y,z
-output_struct.kpsx = output_struct.k_p * output_struct.sigma_x/1e6;
-output_struct.kpsy = output_struct.k_p * output_struct.sigma_y/1e6;
-output_struct.kpsz = output_struct.k_p * output_struct.sigma_z/1e6;
+param_struct.beam.kpsx = param_struct.beam.sigma_x/param_struct.plasma.SD; % beam length X [skin depths]
+param_struct.beam.kpsy = param_struct.beam.sigma_y/param_struct.plasma.SD; % beam length Y [skin depths]
+param_struct.beam.kpsz = param_struct.beam.sigma_z/param_struct.plasma.SD; % beam length Z [skin depths]
 
 % Calc peak gaussian current
-output_struct.I_peak = 1e3*input_struct.N_particles*SI_e*SI_c/(sqrt(2*pi)*output_struct.sigma_z); % [kA]
-output_struct.N_particles   = input_struct.N_particles;
+param_struct.beam.I_peak = 1e3*param_struct.beam.N_particles*...
+    SI_e*SI_c/(sqrt(2*pi)*param_struct.beam.sigma_z);            % beam current [kA]
 
 % Calc max bubble radius
-output_struct.R_bubble = 1e6*(1/0.84)*2/output_struct.k_p*sqrt(output_struct.I_peak/17); % [um]
+param_struct.plasma.R_bubble = (1/0.84)*2*param_struct.plasma.SD*...
+    sqrt(param_struct.beam.I_peak/17);                               % plasma bubble radius[um]
+
+
+
+%%%%%%%%%%%%%%%%%%%
+% SIZE ATTRIBUTES %
+%%%%%%%%%%%%%%%%%%%
 
 % Determine simulation box size
-Box_X_exact = max(input_struct.N_bubbleradius*output_struct.R_bubble, input_struct.N_bunchradius*output_struct.sigma_x);  % [um]
-Box_Y_exact = max(input_struct.N_bubbleradius*output_struct.R_bubble, input_struct.N_bunchradius*output_struct.sigma_y);  % [um]
-Box_Z_exact = max(input_struct.N_bunchlengths*output_struct.sigma_z,  input_struct.N_wavelengths*output_struct.lambda_p); % [um]
+Box_X_exact = max(input_struct.size.X_bubbles*param_struct.plasma.R_bubble,...
+    input_struct.size.X_bunches*param_struct.beam.sigma_x);                     % [um]
+Box_Y_exact = max(input_struct.size.X_bubbles*param_struct.plasma.R_bubble,...
+    input_struct.size.X_bunches*param_struct.beam.sigma_x);                     % [um]
+Box_Z_exact = max(input_struct.size.Z_waves*param_struct.plasma.lambda_p,...
+    input_struct.size.Z_bunches*param_struct.beam.sigma_z);                    % [um]
 
 % Round to nearet micron
-output_struct.Box_X = ceil(Box_X_exact/10)*10; % [um]
-output_struct.Box_Y = ceil(Box_Y_exact/10)*10; % [um]
-output_struct.Box_Z = ceil(Box_Z_exact/10)*10; % [um]
+param_struct.size.Box_X = ceil(Box_X_exact/10)*10; % [um]
+param_struct.size.Box_Y = ceil(Box_Y_exact/10)*10; % [um]
+param_struct.size.Box_Z = ceil(Box_Z_exact/10)*10; % [um]
 
 % Determine grid spacing in terms of skin depth
 skin_frac = 1/20;
 
 % Determine number of cells, must be at least 2^6 per dim
-output_struct.INDX = max(6, floor( log(output_struct.Box_X*1e-6 * output_struct.k_p / skin_frac) / log(2) ));
-output_struct.INDY = max(6, floor( log(output_struct.Box_Y*1e-6 * output_struct.k_p / skin_frac) / log(2) ));
-output_struct.INDZ = max(6, floor( log(output_struct.Box_Z*1e-6 * output_struct.k_p / skin_frac) / log(2) ));
+param_struct.size.INDX = max(6,floor(log(param_struct.size.Box_X*1e-6*...
+    param_struct.plasma.k_p/skin_frac)/log(2)));
+param_struct.size.INDY = max(6,floor(log(param_struct.size.Box_Y*1e-6*...
+    param_struct.plasma.k_p/skin_frac)/log(2)));
+param_struct.size.INDZ = max(6,floor(log(param_struct.size.Box_Z*1e-6*...
+    param_struct.plasma.k_p/skin_frac)/log(2)));
 
 % Increase granularity if desired
-output_struct.INDX = output_struct.INDX + input_struct.x_grain;
-output_struct.INDY = output_struct.INDY + input_struct.y_grain;
-output_struct.INDZ = output_struct.INDZ + input_struct.z_grain;
+param_struct.size.INDX = param_struct.size.INDX+input_struct.size.x_grain;
+param_struct.size.INDY = param_struct.size.INDY+input_struct.size.y_grain;
+param_struct.size.INDZ = param_struct.size.INDZ+input_struct.size.z_grain;
 
 % Determine cell size
-output_struct.Cell_X = output_struct.Box_X/(2^(output_struct.INDX)); % [um]
-output_struct.Cell_Y = output_struct.Box_Y/(2^(output_struct.INDY)); % [um]
-output_struct.Cell_Z = output_struct.Box_Z/(2^(output_struct.INDZ)); % [um]
+param_struct.size.Cell_X = param_struct.size.Box_X/(2^(param_struct.size.INDX)); % [um]
+param_struct.size.Cell_Y = param_struct.size.Box_Y/(2^(param_struct.size.INDY)); % [um]
+param_struct.size.Cell_Z = param_struct.size.Box_Z/(2^(param_struct.size.INDZ)); % [um]
 
 % Do not allow transverse cell size to be smaller than longitudinal cell size
-while output_struct.Cell_Z > output_struct.Cell_X
-   output_struct.INDZ = output_struct.INDZ + 1;
-   output_struct.Cell_Z = output_struct.Box_Z/(2^(output_struct.INDZ));
+while param_struct.size.Cell_Z > param_struct.size.Cell_X
+   param_struct.size.INDZ = param_struct.size.INDZ + 1;
+   param_struct.size.Cell_Z = param_struct.size.Box_Z/(2^(param_struct.size.INDZ));
 end
+
+% Determine number of grid points
+param_struct.size.Grid_X = 2^param_struct.size.INDX;
+param_struct.size.Grid_Y = 2^param_struct.size.INDY;
+param_struct.size.Grid_Z = 2^param_struct.size.INDZ;
 
 % Cell Fraction
-output_struct.Frac_X = output_struct.Cell_X/output_struct.SD;
-output_struct.Frac_Z = output_struct.Cell_Z/output_struct.SD;
+param_struct.size.Frac_X = param_struct.size.Cell_X/param_struct.plasma.SD;
+param_struct.size.Frac_Y = param_struct.size.Cell_Y/param_struct.plasma.SD;
+param_struct.size.Frac_Z = param_struct.size.Cell_Z/param_struct.plasma.SD;
 
-% Place beam on even/odd cell
-output_struct.Box_X = output_struct.Box_X + input_struct.even_odd;
-output_struct.Box_Y = output_struct.Box_Y + input_struct.even_odd;
 
-% Determine beam position
-output_struct.beam_x_pos = round(output_struct.Box_X*input_struct.x_frac);
-output_struct.beam_y_pos = round(output_struct.Box_Y*input_struct.y_frac);
-output_struct.beam_z_pos = round(output_struct.Box_Z*input_struct.z_frac);
+
+%%%%%%%%%%%%%%%%%%%
+% TIME ATTRIBUTES %
+%%%%%%%%%%%%%%%%%%%
 
 % Determine slice time
-output_struct.DT = round(sqrt(2*output_struct.gamma)/10 / 1.5); % 1.5 is deceleration factor
-if( input_struct.BEAM_EV )
-  output_struct.TEND       = floor(input_struct.plasma_s_prop / (SI_c / output_struct.omega_p))+0.1;
-  output_struct.DT_OUTPUT  = 10; % output data every n'th timestep
-  output_struct.DT_RESTART = output_struct.DT_OUTPUT*4;
+param_struct.time.DT = round(sqrt(2*param_struct.beam.gamma)/10 / 1.5); % 1.5 is deceleration factor
+if( input_struct.sim.BEAM_EV )
+  param_struct.time.TEND       = floor(input_struct.sim.prop / (SI_c / param_struct.plasma.omega_p))+0.1;
+  param_struct.time.DT_OUTPUT  = 10; % output data every n'th timestep
+  param_struct.time.DT_RESTART = param_struct.time.DT_OUTPUT*4;
 else
-  output_struct.TEND       = output_struct.DT+0.1;
-  output_struct.DT_OUTPUT  = 1; % output data every timestep
-  output_struct.DT_RESTART = 0;
+  param_struct.time.TEND       = param_struct.time.DT+0.1;
+  param_struct.time.DT_OUTPUT  = 1; % output data every timestep
+  param_struct.time.DT_RESTART = 0;
 end
 
-% copy over remaining variables
-output_struct.plasma_PREION = input_struct.plasma_PREION;
-output_struct.plasma_Z      = input_struct.plasma_Z;
-output_struct.charge        = input_struct.charge;
-output_struct.mass          = input_struct.mass;
-output_struct.store_QEB_3D  = input_struct.store_QEB_3D;
 
 
-% Dump values?
-if 0
-   output_struct
-end
+%%%%%%%%%%%%%%%%%%%%%%%
+% POSITION ATTRIBUTES %
+%%%%%%%%%%%%%%%%%%%%%%%
+
+% Determine beam position
+param_struct.pos.X_center = round(param_struct.size.Box_X*input_struct.size.X_center);
+param_struct.pos.Y_center = round(param_struct.size.Box_Y*input_struct.size.Y_center);
+param_struct.pos.Z_center = round(param_struct.size.Box_Z*input_struct.size.Z_center);
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+% Diagnostic ATTRIBUTES %
+%%%%%%%%%%%%%%%%%%%%%%%%%
+
+param_struct.diag.store_QEB_3D  = input_struct.diag.store_QEB_3D;
--- WRITE_RP.m
+++ WRITE_RP.m
@@ -114,9 +114,9 @@ while( row ~= -1 )
 
     % SIMSYS SECTION
     elseif( ~isempty(strfind(row, 'Box_X') ) && section_simsys == 1)
-       fprintf(fidout, [' Box_X=' num2str(input_struct.Box_X) ', Box_Y=' num2str(input_struct.Box_Y) ', Box_Z=' num2str(input_struct.Box_Z) ',' '\n']  );
+       fprintf(fidout, [' Box_X=' num2str(input_struct.size.Box_X) ', Box_Y=' num2str(input_struct.size.Box_Y) ', Box_Z=' num2str(input_struct.size.Box_Z) ',' '\n']  );
     elseif( ~isempty(strfind(row, 'INDX') ) && section_simsys == 1)
-       fprintf(fidout, [' INDX =  ' num2str(input_struct.INDX) ' , INDY = ' num2str(input_struct.INDY) ', INDZ = ' num2str(input_struct.INDZ) '\n']  );
+       fprintf(fidout, [' INDX =  ' num2str(input_struct.size.INDX) ' , INDY = ' num2str(input_struct.size.INDY) ', INDZ = ' num2str(input_struct.size.INDZ) '\n']  );
 
     % NUMBEAMS SECTION
     elseif( ~isempty(strfind(row, 'NBeams') ) && section_nbeams == 1)
@@ -130,16 +130,16 @@ while( row ~= -1 )
           fprintf(fidout, [' BEAM_EVOLUTION = .true.' '\n']  );
           fprintf(fidout, [' MIN_BEAM_PARTICLE = 8' '\n']  );
           fprintf(fidout, [' NPX =  ' num2str(2^7) ', NPY = ' num2str(2^7) ', NPZ = ' num2str(2^8) '\n']);
-          fprintf(fidout, [' Charge = ' num2str(input_struct.charge, '%.1f') '\n']  );
-          qp_par_mass_str = num2str(input_struct.mass, '%.3G');
+          fprintf(fidout, [' Charge = ' num2str(input_struct.beam.charge, '%.1f') '\n']  );
+          qp_par_mass_str = num2str(input_struct.beam.mass, '%.3G');
           qp_par_mass_str = strrep(qp_par_mass_str,'E+0','E');
           qp_par_mass_str = strrep(qp_par_mass_str,'E+','E');
           fprintf(fidout, [' Mass = ' qp_par_mass_str '\n']  );
-          qp_gam_part_str = num2str(input_struct.gamma, '%.3G');
+          qp_gam_part_str = num2str(input_struct.beam.gamma, '%.3G');
           qp_gam_part_str = strrep(qp_gam_part_str,'E+0','E');
           qp_gam_part_str = strrep(qp_gam_part_str,'E+','E');
           fprintf(fidout, [' Gamma = ' qp_gam_part_str ',' '\n']  );
-          qp_num_part_str = num2str(input_struct.N_particles, '%.3G');
+          qp_num_part_str = num2str(input_struct.beam.N_particles, '%.3G');
           qp_num_part_str = strrep(qp_num_part_str,'E+0','E');
           qp_num_part_str = strrep(qp_num_part_str,'E+','E');
           fprintf(fidout, [' Num_Particle = ' qp_num_part_str ',' '\n']);
@@ -147,9 +147,9 @@ while( row ~= -1 )
           fprintf(fidout, [' Init_Routine = 1' '\n']  );
           fprintf(fidout, [' BEAM_PROFILE = ''test.hdf''' '\n']  );
           fprintf(fidout, [' QUIET_START = .true.' '\n']  );
-          fprintf(fidout, [' Parameter_Array(1:1,1:3) = ' num2str(input_struct.beam_x_pos, '%.2f') ',' num2str(input_struct.beam_y_pos, '%.2f')  ',' num2str(input_struct.beam_z_pos, '%.2f') '\n']);
-          fprintf(fidout, [' Parameter_Array(2:2,1:3) = ' num2str(input_struct.sigma_x, '%.2f') ',' num2str(input_struct.sigma_y, '%.2f')  ',' num2str(input_struct.sigma_z, '%.2f') '\n']);
-          fprintf(fidout, [' Parameter_Array(3:3,1:3) = ' num2str(input_struct.emit_x, '%.3f') ',' num2str(input_struct.emit_y, '%.3f') ',' num2str(0., '%.3f')  '\n'] );
+          fprintf(fidout, [' Parameter_Array(1:1,1:3) = ' num2str(input_struct.pos.X_center, '%.2f') ',' num2str(input_struct.pos.Y_center, '%.2f')  ',' num2str(input_struct.pos.Z_center, '%.2f') '\n']);
+          fprintf(fidout, [' Parameter_Array(2:2,1:3) = ' num2str(input_struct.beam.sigma_x, '%.2f') ',' num2str(input_struct.beam.sigma_y, '%.2f')  ',' num2str(input_struct.beam.sigma_z, '%.2f') '\n']);
+          fprintf(fidout, [' Parameter_Array(3:3,1:3) = ' num2str(input_struct.beam.emit_x, '%.3f') ',' num2str(input_struct.beam.emit_y, '%.3f') ',' num2str(0., '%.3f')  '\n'] );
           fprintf(fidout, [' Parameter_Array(4:4,1:3) = ' num2str(0., '%.3f') ',' num2str(-0., '%.3f') ',' num2str(0., '%.3f')  '\n'] );
           fprintf(fidout, [' Parameter_Array(5:5,1:3) = ' num2str(0., '%.3f') ',' num2str(-0., '%.3f') ',' num2str(0., '%.3f')  '\n'] );
           fprintf(fidout, [' Use_Shifter = .false.' '\n']  );
@@ -160,8 +160,8 @@ while( row ~= -1 )
           fprintf(fidout, [' Use_Destroyer = .true.' '\n']  );
           fprintf(fidout, [' Destroyer_NCriteria = 5' '\n']  );
           fprintf(fidout, [' Destroyer_Criteria(1:1,1:5)=1,1,2,2,6' '\n']  );
-          fprintf(fidout, [' Destroyer_Criteria(2:2,1:5)=0,' num2str(input_struct.Box_X-2, '%.0f') ',0,' num2str(input_struct.Box_Y-2, '%.0f')  ',0' '\n']);
-          fprintf(fidout, [' Destroyer_Criteria(3:3,1:5)=2,' num2str(input_struct.Box_X, '%.0f') ',2,' num2str(input_struct.Box_Y, '%.0f')  ',100' '\n']);
+          fprintf(fidout, [' Destroyer_Criteria(2:2,1:5)=0,' num2str(input_struct.size.Box_X-2, '%.0f') ',0,' num2str(input_struct.size.Box_Y-2, '%.0f')  ',0' '\n']);
+          fprintf(fidout, [' Destroyer_Criteria(3:3,1:5)=2,' num2str(input_struct.size.Box_X, '%.0f') ',2,' num2str(input_struct.size.Box_Y, '%.0f')  ',100' '\n']);
           fprintf(fidout, [' Use_Radiation_Damping = .false.' '\n']  );
           fprintf(fidout, ['/' '\n']  );
       %end% for
@@ -170,18 +170,18 @@ while( row ~= -1 )
 
       % PLASMA SECTION
       elseif( ~isempty(strfind(row, 'Plasma_Density') ) && section_plasma == 1)
-         Plasma_Density_str = num2str(input_struct.plasma_density, '%.3G');
+         Plasma_Density_str = num2str(input_struct.plasma.density, '%.3G');
          Plasma_Density_str = strrep(Plasma_Density_str,'E+0','E');
          Plasma_Density_str = strrep(Plasma_Density_str,'E+','E');
          fprintf(fidout, [' Plasma_Density=' Plasma_Density_str '\n']  );
       elseif( ~isempty(strfind(row, 'Nspecies') ) && section_plasma == 1)
-         if( input_struct.plasma_PREION == 1)
+         if( input_struct.plasma.PREION == 1)
             fprintf(fidout, [' Nspecies=1' '\n']  );
          else
             fprintf(fidout, [' Nspecies=0' '\n']  );
          end% if
       elseif( ~isempty(strfind(row, 'Nneutral') ) && section_plasma == 1)
-         if( input_struct.plasma_PREION == 1)
+         if( input_struct.plasma.PREION == 1)
             fprintf(fidout, [' Nneutrals=0' '\n']  );
          else
             fprintf(fidout, [' Nneutrals=1' '\n']  );
@@ -189,44 +189,44 @@ while( row ~= -1 )
 
       % NEUTRAL SECTION
       elseif( ~isempty(strfind(row, 'Neutral_gas') ) && section_neutral == 1)
-         fprintf(fidout, [' Neutral_gas = ' num2str(input_struct.plasma_Z) '\n']  );
+         fprintf(fidout, [' Neutral_gas = ' num2str(input_struct.plasma.Z) '\n']  );
 
       % SPECIES SECTION 
       elseif( ~isempty(strfind(row, 'NP2') ) && section_species == 1)
          n_plasma_particles_per_cell = 4; % form UCLA
-         fprintf(fidout, [' NP2 = ' num2str(round(sqrt((2^input_struct.INDX)^2 * n_plasma_particles_per_cell))) '\n']  );
+         fprintf(fidout, [' NP2 = ' num2str(round(sqrt((2^input_struct.size.INDX)^2 * n_plasma_particles_per_cell))) '\n']  );
 
       % SIMTIME SECTION
       elseif( ~isempty(strfind(row, 'TEND') ) && section_simtime == 1) 
-         TEND_str = num2str(input_struct.TEND, '%10G');
+         TEND_str = num2str(input_struct.time.TEND, '%10G');
          TEND_str = strrep(TEND_str,'E+0','E');
          TEND_str = strrep(TEND_str,'E+','E');
-         fprintf(fidout, [' TEND =' TEND_str ', DT = ' num2str(input_struct.DT, '%.1f') '  ,' '\n']  );
+         fprintf(fidout, [' TEND =' TEND_str ', DT = ' num2str(input_struct.time.DT, '%.1f') '  ,' '\n']  );
 
       % DIAG SECTION
       elseif( ~isempty(strfind(row, 'DFESLICE') ) && section_fielddiag == 1) 
-         fprintf(fidout, [' DFESLICE=' num2str(input_struct.DT_OUTPUT) ', EX0=0, EY0=' num2str(input_struct.beam_y_pos, '%.0f') ', EZ0=0' '\n']  );
+         fprintf(fidout, [' DFESLICE=' num2str(input_struct.time.DT_OUTPUT) ', EX0=0, EY0=' num2str(input_struct.pos.Y_center, '%.0f') ', EZ0=0' '\n']  );
       elseif( ~isempty(strfind(row, 'DFBSLICE') ) && section_fielddiag == 1) 
-         fprintf(fidout, [' DFBSLICE=' num2str(input_struct.DT_OUTPUT) ', BX0=0, BY0=' num2str(input_struct.beam_y_pos, '%.0f') ', BZ0=0' '\n']  );
+         fprintf(fidout, [' DFBSLICE=' num2str(input_struct.time.DT_OUTPUT) ', BX0=0, BY0=' num2str(input_struct.pos.Y_center, '%.0f') ', BZ0=0' '\n']  );
       elseif( ~isempty(strfind(row, 'DFQEB')) && isempty(strfind(row, 'DFQEBSLICE'))  && section_beamdiag == 1)
-         if(input_struct.store_QEB_3D)
-            fprintf(fidout, [' DFQEB=' num2str(input_struct.DT_OUTPUT) ',' '\n']  );
+         if(input_struct.diag.store_QEB_3D)
+            fprintf(fidout, [' DFQEB=' num2str(input_struct.time.DT_OUTPUT) ',' '\n']  );
          else
             fprintf(fidout, [' DFQEB=0,' '\n']  );
          end% if
       elseif( ~isempty(strfind(row, 'DFQEBSLICE'))  && section_beamdiag == 1)
-         fprintf(fidout, [' DFQEBSLICE=' num2str(input_struct.DT_OUTPUT) ' , QEBX0=0., QEBY0=' num2str(input_struct.beam_y_pos, '%.0f') ',  QEBZ0=0' '\n']  );
+         fprintf(fidout, [' DFQEBSLICE=' num2str(input_struct.time.DT_OUTPUT) ' , QEBX0=0., QEBY0=' num2str(input_struct.pos.Y_center, '%.0f') ',  QEBZ0=0' '\n']  );
       elseif( ~isempty(strfind(row, 'DFQEPSLICE'))  && section_plasmadiag == 1)
-         fprintf(fidout, [' DFQEPSLICE=' num2str(input_struct.DT_OUTPUT) ' , QEPX0= 0, QEPY0=' num2str(input_struct.beam_y_pos, '%.0f') ',  QEPZ0=0' '\n']  );
+         fprintf(fidout, [' DFQEPSLICE=' num2str(input_struct.time.DT_OUTPUT) ' , QEPX0= 0, QEPY0=' num2str(input_struct.pos.Y_center, '%.0f') ',  QEPZ0=0' '\n']  );
       elseif( ~isempty(strfind(row, 'DUMP_PHA_BEAM'))  && section_beamphasediag == 1)
-         fprintf(fidout, [' DUMP_PHA_BEAM=.true., DFPHA_BEAM=' num2str(input_struct.DT_OUTPUT) ',' '\n']  );
+         fprintf(fidout, [' DUMP_PHA_BEAM=.true., DFPHA_BEAM=' num2str(input_struct.time.DT_OUTPUT) ',' '\n']  );
       elseif( ~isempty(strfind(row, 'DSAMPLE_BEAM'))  && section_beamphasediag == 1)
          fprintf(fidout, [' DSAMPLE_BEAM = ' num2str(2^5) '\n']  );
 
       % RESTART SECTION
       elseif( ~isempty(strfind(row, 'DUMP_RST_FILE'))  && section_restart == 1)
-         if( input_struct.DT_RESTART)
-            fprintf(fidout, [' DUMP_RST_FILE = .true.,  DFRST=' num2str(input_struct.DT_RESTART) '\n']  );
+         if( input_struct.time.DT_RESTART)
+            fprintf(fidout, [' DUMP_RST_FILE = .true.,  DFRST=' num2str(input_struct.time.DT_RESTART) '\n']  );
          else
             fprintf(fidout, [' DUMP_RST_FILE = .false.,  DFRST=1' '\n']  );
          end% if
