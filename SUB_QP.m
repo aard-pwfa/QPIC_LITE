154a101e6da395976a860438984397b67f415f98
scorde <sebastien.corde@polytechnique.edu>
2013-05-27 14:50:33 -0700

    uncomment qsub

--- SUB_QP.m
+++ SUB_QP.m
@@ -14,7 +14,7 @@ copyfile([d '/rpinputs/' date_dir 'rpinput_' rpinput_output_name],'rpinput');
 copyfile([d '/params/' date_dir 'param_' rpinput_output_name '.mat'],'param.mat');
 copyfile([d '/commands/' date_dir 'qpic.e.cmd_' rpinput_output_name],'qpic.e.cmd');
 
-%unix('qsub qpic.e.cmd','-echo');
+unix('qsub qpic.e.cmd','-echo');
 
 cd(d);
 exit;
