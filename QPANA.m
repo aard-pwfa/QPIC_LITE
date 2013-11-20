c2c2fc411b5ee480c96a6e467a6695f3dde01167
sgess <spencergessner@gmail.com>
2013-11-19 23:13:18 -0800

    analyzing long plasmas

--- QPANA.m
+++ QPANA.m
@@ -9,11 +9,14 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Name of folder containing data
-name = 'SJG_004';
+name = 'ea2b';
 
 % Set Data and plotting directories
-data_dir = '/Users/sgess/Desktop/FACET/2014/qp_tars/2013/Oct/SJG_004/';
-plot_dir = '/Users/sgess/Desktop/FACET/2014/qp_plots/2013/Oct/SJG_004/';
+%data_dir = '/Users/sgess/Desktop/FACET/2014/qp_tars/2013/Oct/SJG_004/';
+%plot_dir = '/Users/sgess/Desktop/FACET/2014/qp_plots/2013/Oct/SJG_004/';
+
+data_dir = '/Volumes/PWFA_GoFlex/sgess/2014/ea2b/';
+plot_dir = '/Users/sgess/Desktop/plots/QP/2014/ea2b/';
 if ~exist(plot_dir,'dir')
     mkdir(plot_dir);
 end
@@ -26,7 +29,8 @@ save_plot = 0;
 save_ext  = '.pdf';
 
 % Get plasma and beam parameters
-load([data_dir '/param.mat']);
+%load([data_dir 'param.mat']);
+input_struct = 0;
 
 % What data do you want to use?
 QEB_type = 'QEB-XZ';
@@ -37,9 +41,9 @@ FEZ_type = 'FEZ-XZ';
 BEAM = 1;
 
 % Choose time steps to run over
-file_number = 1;
+file_number = 20;
 
-
+for file_number = 20:20:1120
 
 %%%%%%%%%%%%%
 % LOAD DATA %
@@ -67,8 +71,10 @@ end
 % PLOT DATA %
 %%%%%%%%%%%%%
 
-%PLOT_FUN(QEB_type,2,beam_rho,z_axis,x_axis,plot_units,file_number,input_struct,plot_dir,save_plot,save_ext,1);
+PLOT_FUN(QEB_type,2,beam_rho,z_axis,x_axis,plot_units,file_number,input_struct,plot_dir,save_plot,save_ext,1);
 PLOT_FUN(QEP1_type,2,plas_rho,z_axis,x_axis,plot_units,file_number,input_struct,plot_dir,save_plot,save_ext,2);
 PLOT_FUN(FEZ_type,2,field_EZ,z_axis,x_axis,plot_units,file_number,input_struct,plot_dir,save_plot,save_ext,3);
 PLOT_FUN(FEZ_type,1,field_EZ,z_axis,x_axis,plot_units,file_number,input_struct,plot_dir,save_plot,save_ext,4);
+pause(0.2);
+end
 %save('../../../COMPARE/QP_eShort2.mat','ZZ','field_EZ');
